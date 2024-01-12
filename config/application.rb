6ebef9a15070d3331950db49c25560692c95c791
Muhammad Zunair khan <zunairkhan811@gmail.com>
2024-01-12 16:53:00 +0500

    add rendersh and render.yaml file

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,9 @@ module Mysite
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
+    config.assets.precompile += %w( application.css )
+    Rails.application.config.assets.precompile += %w( application.js )
+
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
